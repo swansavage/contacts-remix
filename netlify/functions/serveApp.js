82305c7a8c771d6517ec09845df06e4b0d42e813
swansavage@github.com <swansavage@gmail.com>
2024-05-05 11:32:09 -0500

    initial commit

---
+++ netlify/functions/serveApp.js
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+
+async function handler(event, context) {
+  const path = event.queryStringParameters.path || "index.html"; // Default to 'index.html' if no path is specified
+  const url = `https://dazzling-mermaid-91b987.netlify.app/`;
+
+  try {
+    const response = await fetch(url);
+    const body = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": response.headers.get("Content-Type"), // Mimic the content type of the fetched resource
+      },
+      body: body,
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+}
+
+export { handler };
