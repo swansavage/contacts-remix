04f9d65589911f653f317e83f01006b5424f0357
swansavage@github.com <swansavage@gmail.com>
2024-05-05 13:40:05 -0500

    netlify function troubleshoot - adjust headers

--- netlify/functions/serveApp.js
+++ netlify/functions/serveApp.js
@@ -1,7 +1,21 @@
-const fetch = require("node-fetch");
-
 exports.handler = async function (event, context) {
-  const path = event.queryStringParameters.path || "index.html"; // Default to 'index.html' if no path is specified
+  const headers = {
+    "Access-Control-Allow-Origin":
+      "https://papaya-bellflower-58z8.squarespace.com",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  // Handle OPTIONS request for CORS preflight
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers,
+      body: "",
+    };
+  }
+
+  const path = event.queryStringParameters.path || "index.html";
   const url = `https://elaborate-cactus-791d91.netlify.app/`;
 
   try {
@@ -11,14 +25,12 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": response.headers.get("Content-Type"), // Mimic the content type of the fetched resource
-        "Access-Control-Allow-Origin":
-          "https://papaya-bellflower-58z8.squarespace.com/", // Allow requests from any origin
-        // Add other CORS headers if needed
+        ...headers,
+        "Content-Type": response.headers.get("Content-Type") || "text/html", // Ensuring a default
       },
       body: body,
     };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    return { statusCode: 500, headers, body: error.toString() };
   }
 };
